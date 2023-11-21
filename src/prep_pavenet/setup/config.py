1719058f5234aaaf09bdab6f7c2cee47057464f0
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-21 01:07:55 +0100

    added: constants for the keys and creation of output folders

--- src/prep_pavenet/setup/config.py
+++ src/prep_pavenet/setup/config.py
@@ -1,9 +1,40 @@
+from __future__ import annotations
+
 from pathlib import Path
+from prep_pavenet.common.constants import *
 
 import toml
 
+# Config keys.
 LOG_SETTINGS = "logging"
+TRAFFIC_SETTINGS = "traffic"
 VEHICLE_SETTINGS = "vehicles"
+RSU_SETTINGS = "rsu"
+OUTPUT_SETTINGS = "output"
+CONTROLLER_SETTINGS = "controller"
+
+# Traffic keys.
+NETWORK_FILE = "network"
+TRACE_FILE = "trace"
+OFFSET_X = "offset_x"
+OFFSET_Y = "offset_y"
+
+# Vehicle keys.
+SIMULATOR = "simulator"
+
+# Output keys.
+OUTPUT_PATH = "output_path"
+
+# RSU keys.
+PLACEMENT = "placement"
+START_TIME = "start_time"
+END_TIME = "end_time"
+
+# Logging keys.
+LOG_LEVEL = "log_level"
+LOG_OVERWRITE = "log_overwrite"
+
+ID_INIT = "id_init"
 
 
 def read_config_toml(config_toml: str) -> dict:
@@ -26,3 +57,29 @@ class Config:
         self.config_file: str = config_file
         self.path: Path = Path(config_file).parent
         self.settings: dict = read_config_toml(config_file)
+        self._create_output_folders()
+
+    def _create_output_folders(self) -> None:
+        """Create the output folders."""
+        output_path = (
+            Path(self.config_file).parent / self.settings[OUTPUT_SETTINGS][OUTPUT_PATH]
+        )
+        output_path.mkdir(parents=True, exist_ok=True)
+        Path.mkdir(output_path / ACTIVATIONS_FOLDER, exist_ok=True)
+        Path.mkdir(output_path / LINKS_FOLDER, exist_ok=True)
+        Path.mkdir(output_path / POSITIONS_FOLDER, exist_ok=True)
+
+    def get(self, key: str) -> dict:
+        """Get the configuration data for a specific key.
+
+        Parameters
+        ----------
+        key : str
+            The key for the configuration data.
+
+        Returns
+        -------
+        dict
+            The configuration data for the key.
+        """
+        return self.settings.get(key)
