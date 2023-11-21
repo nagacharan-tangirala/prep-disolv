d6e0bfd16d9ff9b8fd9d08d0d090886a16c50d35
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-21 01:10:22 +0100

    added: creation of controller at the center of the map

---
+++ src/prep_pavenet/controller/central.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from pathlib import Path
+import pandas as pd
+
+from prep_pavenet.setup.config import END_TIME, NETWORK_FILE, START_TIME, ID_INIT
+from prep_pavenet.common.constants import (
+    CONTROLLER_COLUMNS,
+    ACTIVATION_COLUMNS,
+    ACTIVATIONS_FOLDER,
+    POSITIONS_FOLDER,
+)
+from prep_pavenet.common.utils import get_center
+
+CENTRE = "center"
+
+
+class CentralControllerPlacer:
+    def __init__(
+        self,
+        trace_config: dict,
+        cont_config: dict,
+        config_path: Path,
+        output_path: Path,
+    ) -> None:
+        """The constructor of the CentralControllerPlacer class."""
+        self.output_path = output_path
+        self.config_path = config_path
+        self.start_time = cont_config[START_TIME]
+        self.end_time = cont_config[END_TIME]
+        self.id_init = cont_config[ID_INIT]
+        self.sumo_net = config_path / trace_config[NETWORK_FILE]
+
+    def create_controller_data(self):
+        """Create the controller data."""
+        self._write_activation_data()
+        self._write_controller_data()
+
+    def _write_activation_data(self) -> None:
+        """Write the activation data of controller to a file."""
+        activation_file = (
+            self.output_path / ACTIVATIONS_FOLDER / "controller_activations.parquet"
+        )
+        controller_id = self.id_init
+        activation_df = pd.DataFrame(
+            [[controller_id, self.start_time, self.end_time]],
+            columns=ACTIVATION_COLUMNS,
+        )
+        activation_df.to_parquet(activation_file, index=False)
+        activation_df.to_csv(
+            self.output_path / ACTIVATIONS_FOLDER / "controller_activations.csv",
+            index=False,
+            header=True,
+        )
+
+    def _write_controller_data(self) -> None:
+        """Write the controller data to a file."""
+        controller_file = self.output_path / POSITIONS_FOLDER / "controller.parquet"
+        controller_id = self.id_init
+        centers = get_center(self.sumo_net)
+        controller_df = pd.DataFrame(
+            [[0, controller_id, centers[0], centers[1]]],
+            columns=CONTROLLER_COLUMNS,
+        )
+        controller_df.to_parquet(controller_file, index=False)
+        controller_df.to_csv(
+            self.output_path / POSITIONS_FOLDER / "controller.csv",
+            index=False,
+            header=True,
+        )
---
+++ src/prep_pavenet/controller/controller.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import logging
+
+from prep_pavenet.controller.central import CentralControllerPlacer
+from prep_pavenet.setup.config import *
+
+logger = logging.getLogger(__name__)
+
+CENTER = "center"
+
+
+def create_controllers(config: Config) -> None:
+    """Create the controller data."""
+    logger.debug("Read Controller data")
+    output_path = config.path / config.get(OUTPUT_SETTINGS)[OUTPUT_PATH]
+    if config.get(CONTROLLER_SETTINGS)[PLACEMENT] == CENTER:
+        controller_placer = CentralControllerPlacer(
+            config.get(TRAFFIC_SETTINGS),
+            config.get(CONTROLLER_SETTINGS),
+            config.path,
+            output_path,
+        )
+        controller_placer.create_controller_data()
