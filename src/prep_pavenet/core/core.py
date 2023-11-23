95a4e028e45ef05853e57e5f5ab698e40f7a6e4f
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-24 00:20:15 +0100

    added: necessary input values to the node creation functions

--- src/prep_pavenet/core/core.py
+++ src/prep_pavenet/core/core.py
@@ -22,27 +22,35 @@ class Core:
 
     def _prepare_scenario(self) -> None:
         """Prepare the scenario."""
+        total_node_count = 0
         logger.debug("Preparing the Vehicle Data")
-        self._create_vehicle_data()
+        vehicle_count = self._create_vehicle_data()
+        total_node_count += vehicle_count
+        logger.debug(f"Number of vehicles: {vehicle_count}")
+
         logger.debug("Preparing RSU data")
-        self._create_rsu_data()
+        rsu_count = self._create_rsu_data(total_node_count)
+        total_node_count += rsu_count
+        logger.debug(f"Number of RSUs: {rsu_count}")
+
+        logger.debug("Preparing Controller data")
+        self._create_controller_data(total_node_count)
+
+        logger.debug("Scenario is prepared")
         logger.debug("Preparing Base Station data")
         self._create_base_station_data()
-        logger.debug("Preparing Controller data")
-        self._create_controller_data()
-        logger.debug("Scenario is prepared")
 
-    def _create_vehicle_data(self) -> None:
+    def _create_vehicle_data(self) -> int:
         """Create the vehicle data."""
-        create_vehicles(self.config)
+        return create_vehicles(self.config)
 
-    def _create_rsu_data(self) -> None:
+    def _create_rsu_data(self, rsu_init_id: int) -> int:
         """Create the RSU data."""
-        create_rsu(self.config)
+        return create_rsu(self.config, rsu_init_id)
 
-    def _create_controller_data(self) -> None:
+    def _create_controller_data(self, controller_init_id) -> None:
         """Create the controller data."""
-        create_controllers(self.config)
+        return create_controllers(self.config, controller_init_id)
 
     def _create_base_station_data(self) -> None:
         """Create the base station data."""
