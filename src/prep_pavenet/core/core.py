92b8c5385af1f0f09d33da09b64e4f8c9ef650c3
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-24 13:53:45 +0100

    modified: core to save created files

--- src/prep_pavenet/core/core.py
+++ src/prep_pavenet/core/core.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import logging
 
-from prep_pavenet.controller.controller import create_controllers
-from prep_pavenet.rsu.rsu import create_rsu
+from prep_pavenet.controller.controller import ControllerConverter
+from prep_pavenet.rsu.rsu import RsuConverter
 from prep_pavenet.setup.config import LOG_SETTINGS, Config
 from prep_pavenet.setup.logger import setup_logging
-from prep_pavenet.vehicle.vehicle import create_vehicles
+from prep_pavenet.vehicle.vehicle import VehicleConverter
 
 logger = logging.getLogger(__name__)
 
@@ -14,43 +14,63 @@ logger = logging.getLogger(__name__)
 class Core:
     def __init__(self, config_file: str):
         self.config = Config(config_file)
+        self.vehicle_file = None
+        self.rsu_file = None
+        self.controller_file = None
 
     def prepare_scenario(self) -> None:
         setup_logging(self.config.path, self.config.settings.get(LOG_SETTINGS))
-        logger.debug("Initializing the scenario preparation")
+        logger.info("Initializing the scenario preparation")
         self._prepare_scenario()
 
     def _prepare_scenario(self) -> None:
         """Prepare the scenario."""
         total_node_count = 0
-        logger.debug("Preparing the Vehicle Data")
+        logger.info("Preparing the Vehicle Data")
         vehicle_count = self._create_vehicle_data()
         total_node_count += vehicle_count
-        logger.debug(f"Number of vehicles: {vehicle_count}")
+        logger.info(f"Number of vehicles: {vehicle_count}")
 
-        logger.debug("Preparing RSU data")
+        logger.info("Preparing RSU data")
         rsu_count = self._create_rsu_data(total_node_count)
         total_node_count += rsu_count
-        logger.debug(f"Number of RSUs: {rsu_count}")
+        logger.info(f"Number of RSUs: {rsu_count}")
 
-        logger.debug("Preparing Controller data")
+        logger.info("Preparing Controller data")
         self._create_controller_data(total_node_count)
 
-        logger.debug("Scenario is prepared")
-        logger.debug("Preparing Base Station data")
+        logger.info("Preparing Base Station data")
         self._create_base_station_data()
+        logger.info("Scenario is prepared")
+
+        logger.info("Creating the links")
+        self._create_links()
 
     def _create_vehicle_data(self) -> int:
         """Create the vehicle data."""
-        return create_vehicles(self.config)
+        vehicle_converter = VehicleConverter(self.config)
+        vehicle_converter.create_vehicles()
+        self.vehicle_file = vehicle_converter.vehicle_file
+        return vehicle_converter.vehicle_count
 
     def _create_rsu_data(self, rsu_init_id: int) -> int:
         """Create the RSU data."""
-        return create_rsu(self.config, rsu_init_id)
+        rsu_converter = RsuConverter(self.config)
+        rsu_converter.create_rsu(rsu_init_id)
+        self.rsu_file = rsu_converter.rsu_file
+        return rsu_converter.rsu_count
 
     def _create_controller_data(self, controller_init_id) -> None:
         """Create the controller data."""
-        return create_controllers(self.config, controller_init_id)
+        controller_converter = ControllerConverter(self.config)
+        controller_converter.create_controllers(controller_init_id)
+        self.controller_file = controller_converter.controller_file
+        return controller_converter.controller_count
 
     def _create_base_station_data(self) -> None:
         """Create the base station data."""
+        pass
+
+    def _create_links(self) -> None:
+        """Create the links."""
+        logger.info("Creating the links")
