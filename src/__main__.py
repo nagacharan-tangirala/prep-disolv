ff1a3a7419b0bb3aa405881908b5dfab938b3a0f
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2024-01-05 10:20:53 +0100

    added: execution time is calculated for the scenario generation

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import argparse
 import sys
+import time
 
 from prep_pavenet.core.core import Core
 
@@ -11,7 +12,11 @@ def main():
     parser.add_argument("-c", "--config", help="Config file path", required=True)
     args = parser.parse_args()
     core = Core(args.config)
+    # calculate execution time
+    exec_start = time.time()
     core.prepare_scenario()
+    exec_end = time.time()
+    print(f"Execution time: {exec_end - exec_start} seconds")
 
 
 if __name__ == "__main__":
