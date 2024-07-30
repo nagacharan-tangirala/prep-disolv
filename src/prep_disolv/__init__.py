30633d6ce450bebada067d602e53b23a5be0b83a
charan <nagacharan.tangirala@tum.de>
2024-07-30 16:51:04 +0200

    modified: minor config changes

--- src/prep_disolv/__init__.py
+++ src/prep_disolv/__init__.py
@@ -6,7 +6,3 @@ prep-pavenet: The scenarios for PAVeNet are prepared by this project.
 
 
 from __future__ import annotations
-
-from ._version import version as __version__
-
-__all__ = ["__version__"]
